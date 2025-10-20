e746350c1b86e03dcc3e29aae055ccbbf140b8df
Daniel Szota <szootnikus@gmail.com>
2025-10-20 19:36:23 +0200

    dodanie debugu
    dupa

--- sceny/world.gd
+++ sceny/world.gd
@@ -2,8 +2,25 @@ extends Node3D
 @onready var player: CharacterBody3D = $Player
 
 @onready var puszka_point = $PuszkaPoint
+@onready var debug_window = $DebugLayer/DebugWindow
 
-
+var was_blocked : bool = false
 func _on_player_mam_puszke() -> void:
 	puszka_point.show()
 	puszka_point.be_interactable(true)
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("debug"):
+		debug_window.visible = !debug_window.visible
+		if player:
+			if !player.mouse_block:
+				player.mouse_block = true
+				was_blocked = false
+			elif player.mouse_block:
+				was_blocked = true
+			
+		if debug_window.visible:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED) 
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			
